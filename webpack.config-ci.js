3b048bc3e2ef6accb43e81ae7af37c5b0085af18
Mateusz Goślinowski <mateusz.goslinowski@gmail.com>
2025-01-19 23:38:02 +0100

    Add deploy pipeline

--- webpack.config-ci.js
+++ webpack.config-ci.js
@@ -58,19 +58,6 @@ module.exports = {
     [ new HtmlWebpackPlugin({
         title: 'Halogen Material Components'
     })
-    , new CompressionPlugin({
-        filename: "[path][base].br",
-        algorithm: "brotliCompress",
-        test: /\.(js|css|html|svg)$/,
-        compressionOptions: {
-          params: {
-            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-          },
-        },
-        threshold: 10240,
-        minRatio: 0.8,
-        deleteOriginalAssets: false,
-      }),
     , new webpack.ProgressPlugin()
     ]
 };
